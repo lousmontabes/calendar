8bdc20c2979772996cddb12f44a8e15981d0ed61
Lluís Montabes <lluis.456@gmail.com>
2019-02-25 00:48:56 +0100

    General restyling

---
+++ index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+      <meta charset="utf-8">
+
+      <title>Calendar</title>
+      <meta name="description" content="Calendar">
+      <meta name="author" content="Lluís Montabes">
+
+      <link rel="stylesheet" href="styles/style.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <div class="area">
+
+        <div class="section title">
+            <h1>Calendari de migracions</h1>
+        </div>
+
+        <div class="section entry today">
+            <h2><b>Today</b></h2>
+            <div class="event">
+                12:00 - Juice
+            </div>
+            <div class="event">
+                11:00 - Pedro
+            </div>
+            <div class="event last">
+                11:00 - Vitor
+            </div>
+            <div class="event last">
+                11:00 - Vitor
+            </div>
+        </div>
+
+        <div class="section entry">
+            <h2><b>Dimecres</b>, 20 de febrer</h2>
+            <div class="event">
+                11:00 - Juice
+                <div class="comments">
+                    Cal copiar dades de l'usuari.<br />Avisar a secretaria
+                </div>
+            </div>
+            <div class="event">
+                11:00 - Pedro
+            </div>
+            <div class="event">
+                12:00 - Pedro
+            </div>
+            <div class="event last">
+                12:00 - Vitor
+            </div>
+        </div>
+
+        <div class="section entry">
+            <h2><b>Dijous</b>, 21 de febrer</h2>
+            <div class="event">
+                12:00 - Juice
+            </div>
+            <div class="event">
+                11:00 - Pedro
+            </div>
+            <div class="event last">
+                11:00 - Vitor
+            </div>
+        </div>
+
+        <div class="section new_entry">
+            ＋ Add entry
+        </div>
+
+    </div>
+
+    <script src="js/scripts.js"></script>
+</body>
+</html>
